7065732484dd8f8770b1c56928cde4ec1c7c1119
Porter <lgporter@email.neit.edu>
2023-01-12 14:22:04 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();    //sets to an object instance    //object instantiation in JS
+
+//setup a route for static files
+app.use(express.static(__dirname+"/static"));
+
+//place a request with a designated response on access - ROUTE
+app.get("/", function(req, res){
+    //res.send("Welcome to Web API Games Server");
+    res.sendFile(__dirname+"/static/index.html");
+});
+
+//Route for a home section
+app.get("/home", function(req, res){
+    res.send("This will be our homepage for now.");
+});
+
+app.listen(3000, function(){
+    console.log("Server is running... Please remember to shut down the server... Shutdown with: Control+C");
+});   //listens on port 3000
